81aea9d4597e1bd78584358df093b0bf6615083d
agigli <agigli@iuvity.net>
2023-05-26 21:24:09 -0300

    [ADD] Multiple new lessons of Typescript  - Union & aliases  - Unknown, Never & Void

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log("".concat(resultPhrase + result));
-    }
-    else {
-        return n1 + n2;
-    }
+function add(n1, n2) {
+    return n1 + n2;
 }
-;
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-add(number1, number2, printResult, resultPhrase);
+function printResult(num) {
+    // TOKNOW: VOID return declaration is used when you have a function that doesn't return anything.
+    console.log('Result: ' + num);
+}
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+// combineValues = printResult
+//combineValues = 5
+console.log(combineValues(8, 8));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Agustin';
+if (typeof userInput === 'string') {
+    //TOKNOW: Here don't get the error, because we are under a condition where if the userInput is of type string
+    // then we could make te assignment, TS can handle this kind of uses cases. We have the guarantee that 'userName'
+    // could be correctly assign by userInput.
+    userName = userInput;
+}
+// TOKNOW: Here we can have an error because we don't know which type of data we are going to assing, 
+// is not consistant, so TypeScript can know and handle an error with this tipe of data.
+// userName = userInput
+// TOKNOW: We can use this return type 'NEVER' only when we know that the function is never going to return anything
+// this is because in this example, we are throwing an error wich stops the execution code.
+function generateError(message, code) {
+    throw { puto: message, errorCode: code };
+}
+generateError('An error occurred!', 500);
