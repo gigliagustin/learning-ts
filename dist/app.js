9d73f09746c549fc9d072140dae3e5969ddcdfd2
agigli <agigli@iuvity.net>
2023-09-26 22:40:13 -0300

    [FEATURE] Add interfaces for typescript to learned folder

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,2 @@
 "use strict";
-class Person {
-    constructor(n) {
-        this.age = 30;
-        this.name = n;
-    }
-    greet(phrase) {
-        console.log(`${phrase} ${this.name}`);
-    }
-}
-let user1;
-user1 = new Person('Agucho');
-user1.greet('Hi there - I am');
-console.log(user1);
 //# sourceMappingURL=app.js.map
